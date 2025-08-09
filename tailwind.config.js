872b394d555f69a4e5af10f435fcf3b870a0b613
Leelaprasad <127936392+LEELA-96@users.noreply.github.com>
2025-08-09 13:14:52 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,8 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        accent: '#29f0c1',
-        accent2: '#5ad1ff',
-        bgdark: '#081018',
-        panel: '#0d1115',
-        muted: '#9aa6b2',
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        mono: ['Roboto Mono', 'monospace'],
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
